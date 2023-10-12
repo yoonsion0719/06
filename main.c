fbcfa55a0c7b280eb589fd43fa4825eec540186b
yoonsion0719 <yoonsonia0719@gmail.com>
2023-10-12 17:06:39 +0900

    06-4

--- main.c
+++ main.c
@@ -5,39 +5,19 @@
 
 
 
-int factorial(int n) {
-	int res =1;
-	int i;
-	for (i=1; i<=n; i++)
-		res*=i;
-	return res;
+int square (int a) {
+	return (a*a);
 }
 
 
 
-int combination(int n, int r) {
-	
-	int p,q; //각각 분자 분모
-	p=factorial(n);
-	q=factorial(n-r) * factorial(r);
-	return p/q;
-	
-}
 
 
 
-int main(void) {
+int main() {
 	
-	int n,r;
-	
-	//정수 입력 받기 
-	printf("input n and r : ");
-	scanf("%d %d", &n,&r);
-	
-	//
-	int result;
-	result=combination(n,r);
-	printf("combination(n,r) is %d", result);
-	
+	int a=2;
+	a= square(a);
+	printf("a=%i\n",a);
 	
 }
